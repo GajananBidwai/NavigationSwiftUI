ee59093bf81b171714a8448fa7f1dac23e5ee426
Gajanan Bidwai <gajanan@vyom.ai>
2025-12-05 11:59:33 +0530

    Initial Commit

---
+++ NavigationSwiftUI/NavigationSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationSwiftUI
+//
+//  Created by Neosoft on 05/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationSwiftUI/NavigationSwiftUI/NavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSwiftUIApp.swift
+//  NavigationSwiftUI
+//
+//  Created by Neosoft on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
